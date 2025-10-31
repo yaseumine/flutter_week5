62f94e6ef9747b2da5bf1c4bdab9c5a14043109b
yaseumine <yasminmaharani123@gmail.com>
2025-10-31 08:43:53 +0700

    membuat folder flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screen/splash_screen_1.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "1123150146",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+      ),
+      home: SplashScreen1(),
+    );
+  }
+}
